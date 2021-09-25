017303c3530ddaf471ff58e294ba382df4deca33
Nikita <kravtsovnikitast@mail.ru>
2021-09-25 16:32:23 +0300

    start_branch v0.1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Главная</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
+    <?php require "navbar.html"?>
+  </head>
+  <body>
+
+<p></p>
+  </body>
+</html>
+<!-- https://www.youtube.com/watch?v=eCItZh6uMVc&t=1842s
+    https://www.youtube.com/watch?v=SPWEJTTsFj0&t=145s
+-->
