fb64b688f3fa916e75a4fd3b63fe29b077b9b67b
aleksey-anisimov5568 <63207078+aleksey-anisimov5568@users.noreply.github.com>
2021-10-23 21:53:18 +0300

    Add files via upload

---
+++ blocks/navbar.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">Tickets</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <?php
+      $uri_current = $_SERVER[REQUEST_URI];
+      $main_page = "/index.php";
+      $authorization_page = "/authorization.php";
+      $register_page = "/register.php";
+      switch ($uri_current) {
+        case $main_page:
+          echo '<a class="nav-link active" aria-current="page" href="index.php">Главная</a>';
+          echo '<a class="nav-link" href="register.php">Регистрация</a>';
+          echo '<a class="nav-link" href="authorization.php">Авторизация</a>';
+          break;
+          case $authorization_page:
+            echo '<a class="nav-link" aria-current="page" href="index.php">Главная</a>';
+            echo '<a class="nav-link" href="register.php">Регистрация</a>';
+            echo '<a class="nav-link active" href="authorization.php">Авторизация</a>';
+            break;
+          case $register_page:
+            echo '<a class="nav-link" aria-current="page" href="index.php">Главная</a>';
+            echo '<a class="nav-link active" href="register.php">Регистрация</a>';
+            echo '<a class="nav-link" href="authorization.php">Авторизация</a>';
+            break;
+        default:
+          echo "n";
+          break;
+}
+      ?>
+
+
+
+
+    </div>
+  </div>
+</div>
+</nav>
--- index.php
+++ index.php
@@ -4,9 +4,15 @@
     <meta charset="utf-8">
     <title>Главная</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
-    <?php require "navbar.php"?>
+    <link rel="stylesheet" href="Documents/css/style.css">
+    <?php require "blocks/navbar.php"?>
   </head>
   <body>
+    <div class="container mt-5">
+      <div class="mb-5">
+      <h3>Концерты</h3>
+    </div>
+    <?php require "blocks/glav.php"?>
 <p></p>
   </body>
 </html>
